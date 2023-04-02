43737473538db64bcdbec42aec5ba4fa99aa98d1
Ilya <mazurok.german@gmail.com>
2023-04-02 18:36:16 +0300

    added links for more information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bill Gates';
+
   quotes: string[] = ['"Витрачати гроші розумно настільки ж складно, як і заробляти їх."',
   '"Я зачісуюся кожного разу перед тим, як надіслати e-mail, оскільки сподіваюся виглядати привабливо."',
   '"Успіх нічому не навчає. Він тільки переконує розумних людей у тому, що вони не можуть програти"'];
+
+  links: [string,string][]=[["https://uk.wikipedia.org/wiki/Білл_Гейтс","wikipedia.org"],
+  ["https://www.gatesnotes.com","gatesnotes.com"],
+  ["https://www.facebook.com/BillGates/?locale=ru_RU","facebook.com"]];
 }
