24f91e9ec576e5111a15eaa4e3d460cb9b63b4fd
Ilya <mazurok.german@gmail.com>
2023-04-02 18:25:12 +0300

    add biography Bill Gates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular_Lab_1';
+  title = 'Bill Gates';
+  quotes: string[] = ['"Витрачати гроші розумно настільки ж складно, як і заробляти їх."',
+  '"Я зачісуюся кожного разу перед тим, як надіслати e-mail, оскільки сподіваюся виглядати привабливо."',
+  '"Успіх нічому не навчає. Він тільки переконує розумних людей у тому, що вони не можуть програти"'];
 }
